fa5cc01376d6d68fcabb01d23636fc9c8e72dc2a
Mpendulo1 <mpendulokhozamk2@gmail.com>
2021-09-02 16:26:49 +0200

    updated changes

---
+++ JS/edit.js
@@ -0,0 +1,94 @@
+
+
+function addVehicle() {
+    fetch('https://obscure-sea-63906.herokuapp.com/create-vehicles', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: document.getElementById('name').value,
+            brand: document.getElementById('brand').value,
+            type: document.getElementById('type').value,
+            year: document.getElementById('year').value,
+            description: document.getElementById('description').value,
+            transition: document.getElementById('transtion').value,
+            image: document.querySelector('.image_url').value
+
+        }),
+        mode: 'cors'
+    }).then(res => res.json()).then(data => {
+        console.log(data)
+        console.log("You added a product successfully")
+
+        if (data['message'] == "Product Added Successfully") {
+            alert('You have sucessfully Added A Product! Please View It On The Products Page')
+            window.location.href = './Shop.html'
+        } else {
+            alert('Form filled in incorrectly')
+        }
+    })
+}
+
+
+
+
+// Delete Function
+function deleteProduct() {
+    const productID = document.getElementById('delete-car').value;
+    console.log(productID)
+
+    if (typeof(productID) === "string") {
+        return alert('Please Use Correct Values for Each Section')
+    }
+
+    fetch('https://obscure-sea-63906.herokuapp.com/delete-vehicle/' + `${ productID }`, {
+        method: 'POST',
+    }).then(res => res.json()).then(data => {
+        console.log(data)
+        console.log('You Successfully deleted the product')
+
+        if (data['message'] == 'Product Deleted Successfully') {
+            alert('You successfully deleted the product! Please see confirmation on products page!')
+            window.location.href = '/view-products.html'
+        } else {
+            alert('Error in form! Please revisit')
+        }
+    })
+}
+
+// Add Function
+function updateProducts() {
+    const productID = document.getElementById('Product_ID').value;
+    const name = document.getElementById('name').value;
+    const brand = document.getElementById('brand').value;
+    const type = document.getElementById('type').value;
+    const year = document.getElementById('year').value;
+    const description = document.getElementById('description').value;
+    const transtion = document.getElementById('transition');
+    const image = document.querySelector('.image_url').value;
+
+    fetch('https://obscure-sea-63906.herokuapp.com/edit-vehicle/' + `${ productID }`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            brand: brand,
+            type: type,
+            year: year,
+            description: description,
+            transtion: transtion,
+            image: image
+        })
+    }).then(res => res.json()).then(data => {
+        console.log(data)
+        console.log("You have successfully changed a product")
+
+        if (data['message'] == "Updated Products") {
+            alert('You have successfully updated a product! Please see products page!')
+            window.location.href = './Shop.html'
+        }
+    })
+}
---
+++ JS/register.js
@@ -0,0 +1,44 @@
+function myFunction() {
+    let fname = document.getElementById('fname').value
+    let lname = document.getElementById('lname').value
+    let contact = document.getElementById('contact-input').value
+    let password = document.getElementById('password').value
+    let username = document.getElementById('username').value
+    let email = document.getElementById('email-input').value
+    fetch("https://obscure-sea-63906.herokuapp.com/customer-registration", {
+      method: 'POST', 
+      body: JSON.stringify ({
+        firstname: fname,
+        lastname: lname,
+        contact: contact,
+        password: password,
+        username: username,
+        email: email,
+      }),
+      headers: {
+        "Content-type": "application/json",
+       }})
+       .then((res) => res.json())
+       .then(data => {
+         window.location.replace('./login.html')
+       }) 
+  }
+
+function loginFunction() {
+  let username = document.getElementById('username').value
+  let password = document.getElementById('password').value
+  fetch("https://obscure-sea-63906.herokuapp.com/user-login/", {
+  method: 'PATCH', 
+  body: JSON.stringify ({
+    username: username,
+    password: password,
+  }),
+  headers: {
+    "Content-Type": "application/json",
+     
+  },}) 
+  .then((res) => res.json())
+  .then(data => {
+    window.location.replace('./Shop.html')
+  });
+}
